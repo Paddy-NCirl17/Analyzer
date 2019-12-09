20a293cafa8d58a1ce8ea281c069d3427318b0b0
Patrick Moran <p.jmoran@icloud.com>
2019-12-09 21:49:56 +0000

    added ip dissection

--- Packet_Analyser.py
+++ Packet_Analyser.py
@@ -10,7 +10,7 @@ def main():
         raw_data, addr = conn.recvfrom(65536)
         dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
         print('\nEthernet Frame:')
-        print('Destination: {}, Source: {}, Protocol: {}'.format(dest_mac, src_mac, eth_proto))  
+        print('Destination: {}, Source: {}, ipv4 source {}, ipv4 dest {} Protocol: {}'.format(dest_mac, src_mac,ipv4(src), ipv4(target), eth_proto))  
 
 #Unpack the ethernet frame
 def ethernet_frame(data):
@@ -22,5 +22,15 @@ def get_mac_addr(bytes_addr):
         bytes_str = map('{:02x}'.format, bytes_addr)
         return':'.join(bytes_str).upper()
         
-        
+
+def ipv4_packet(data):
+    version_header_length = data[0]
+    version = version_header_length >> 4
+    header_length =(version_header_length & 15) * 4
+    ttl, proto, src, target, = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
+    return version, header_length, ttl, proto, ipv4(src), ipv4(target), data[header_length:]
+
+def ipv4(addr):
+    return '.'.join(map(str,addr))
+    
 main()        
