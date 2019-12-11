1d5c4584c32a335fcb7d8dab2f56e95d479c552b
Patrick Moran <p.jmoran@icloud.com>
2019-12-11 20:56:07 +0000

    Socket update5

--- Packet_Analyser.py
+++ Packet_Analyser.py
@@ -16,12 +16,7 @@ DATA_TAB_4 = '\t\t\t\t   '
 # Reference: https://youtu.be/_HIefrog_eg
 
 def main():
-    IP = '192.168.28.20'
-    PORT = 5005
     conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
-    conn.bind((IP, PORT))
-    conn.listen(1)
-    conn, addr = s.accept()
     
     while True:
         raw_data, addr = conn.recvfrom(65535)
