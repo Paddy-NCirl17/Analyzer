6b8aad0cc3db66c2bafade4100a307447ab626b1
Patrick Moran <p.jmoran@icloud.com>
2019-12-11 21:57:13 +0000

    Socket updatec

--- Packet_Analyser.py
+++ Packet_Analyser.py
@@ -18,8 +18,7 @@ DATA_TAB_4 = '\t\t\t\t   '
 
 
 def main():
-    udp = socket.getprotobyname("udp")
-    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, udp, socket.ntohs(3))
+    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW,socket.ntohs(3))
     
     while True:
         raw_data, addr = conn.recvfrom(65535)
