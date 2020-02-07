84e6124bdb61aa33a4c7eef118d26232c0f2f2fa
Paddy-NCirl17 <X16102860@STUDENT.NCIRL.IE>
2020-02-07 17:55:06 +0000

    addding inex

--- Packet_Analyser.py
+++ Packet_Analyser.py
@@ -18,11 +18,12 @@ DATA_TAB_4 = '\t\t\t\t   '
 
 
 def main():
-    host = socket.gethostbyname(socket.gethostname())
-    print host
+   # RASP_IP = "192.168.0.10"
+    #RASP_PORT = 5005
 
     conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW,socket.ntohs(3))
-    
+    conn.bind(('eth0', 0))	
+
     while True:
         raw_data, addr = conn.recvfrom(65535)
         dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
@@ -36,10 +37,15 @@ def main():
             print(TAB_2 + 'Protocol: {}, Source: {}, Target: {}'.format(proto, src, target))
 
             if proto == 17:
-               src_port, dest_port, size, data = udp_packet(data)
+               src_port, dest_port, size, control_field, data = udp_packet(data)
                print(TAB_1 + 'UDP Packet:')
-               print(TAB_2 + 'Source Port: {}, Destination Port: {}, Size: {}'.format(src_port, dest_port, size))
-
+               print(TAB_2 + 'Source Port: {}, Destination Port: {}, Size: {}, Control Field: {}'.format(src_port, dest_port, size, control_field))
+               
+               if control_field == 11000000:
+		  stream_id, seq_no, inet_length, ptp_time, data = inetx_packet(data)
+                  print(TAB_1 + 'iNET-X Packet:')
+		  print(TAB_2 + 'Stream ID: {}, Sequence No: {}, iNET-X Length: {}, PTP TimeStamp: {}'.format(stream_id, seq_no, inet_length, ptp_time))
+	 
 #Unpack the ethernet frame
 def ethernet_frame(data):
         dest_mac,src_mac, proto = struct.unpack('! 6s 6s H',data[:14])
@@ -62,10 +68,17 @@ def ipv4(addr):
     return '.'.join(map(str,addr))
     
 def udp_packet(data):
-    src_port, dest_port, size = struct.unpack('! H H 2x H', data[:8])
-    return src_port, dest_port, size, data[8:]
- 
- 
+    src_port, dest_port, size, control_field = struct.unpack('! H H H 2x 4s', data[:12])
+    return src_port, dest_port, size, inet(control_field), data[12:]
+    
+def inet(addr):
+    str = map('{:02x}'.format,addr)
+    return ''.join(str)
+
+def inetx_packet(data):
+    stream_id, seq_no, inet_length, ptp_time = struct.unpack('! 4s 4s 4s 8s', data[:20])
+    return stream_id, seq_no,inet_length, ptp_time , data[20:]    
+    	
 def format_multi_line(prefix, string, size=80):
     size -= len(prefix)
     if isinstance(string, bytes):
