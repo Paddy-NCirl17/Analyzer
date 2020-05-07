c40fd5385346cf061176644080ee2d4c5947289d
Paddy-NCirl17 <X16102860@STUDENT.NCIRL.IE>
2020-05-07 20:06:20 +0100

    Analyzer UPDATE

--- Packet_Analyser.py
+++ Packet_Analyser.py
@@ -18,77 +18,12 @@ import UDP as udp
 import INETX as inetx
 import IENA as iena
 
-<<<<<<< HEAD
 
 broker_address="broker.mqttdashboard.com"
 client = mqtt.Client("PADDY-IOT")
 client.connect(broker_address)
 
  # formatting the MAC Address to AA:BB:CC:DD:EE:FF, for debug only 
-=======
-TAB_1 = '\t - '
-TAB_2 = '\t\t - '
-TAB_3 = '\t\t\t - '
-TAB_4 = '\t\t\t\t - '
-
-DATA_TAB_1 = '\t   '
-DATA_TAB_2 = '\t\t   '
-DATA_TAB_3 = '\t\t\t   '
-DATA_TAB_4 = '\t\t\t\t   '
-
-
-def main():
-   # RASP_IP = "192.168.0.10"
-    #RASP_PORT = 5005
-    operating_system = platform.system()
-    print (operating_system)
-    HOST = socket.gethostbyname(socket.gethostname())
-    print (HOST)
-    
-    if operating_system == 'Windows':
-        conn = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
-        conn.bind((HOST, 0))
-        conn.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-        conn.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-    else:
-        conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW,socket.ntohs(3))
-        conn.bind(('eth0', 0))	
-        
-    while True:
-        if operating_system == 'Linux':
-            raw_data, addr = conn.recvfrom(65535)
-            dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
-            print('\nEthernet Frame:')
-            print(TAB_1 + 'Destination: {}, Source: {}, Protocol: {}'.format(dest_mac, src_mac, eth_proto))  
-
-            if eth_proto == 8 or operating_system == 'Windows':
-                version, header_length, ttl, proto, src, target, data = ipv4_packet(data)
-                print(TAB_1 + 'IPv4 Packet:')
-                print(TAB_2 + 'Version: {}, Header Length: {}, TTL: {},'.format(version, header_length, ttl))
-                print(TAB_2 + 'Protocol: {}, Source: {}, Target: {}'.format(proto, src, target))
-
-                if proto == 17:
-                   src_port, dest_port, size, control_field,udp_data = udp_packet(data)
-                   print(TAB_1 + 'UDP Packet:')
-                   print(TAB_2 + 'Source Port: {}, Destination Port: {}, Size: {}, Control Field: {}'.format(src_port, dest_port, size, control_field))
-
-                   if control_field == '11000000':
-                       stream_id, seq_no, inet_length, ptp_time, data = inetx_packet(udp_data)
-                       print(TAB_1 + 'iNET-X Packet:')
-                       print(TAB_2 + 'Stream ID: {}, Sequence No: {}, iNET-X Length: {}, PTP TimeStamp: {}'.format(stream_id, seq_no, inet_length, ptp_time ))
-
-                   elif control_field != '11000000':
-                         key, size, timestamp, seq, data= iena_packet(data)
-                         print(TAB_1 + 'IENA Packet:')
-                         print(TAB_2 + 'IENA Key: {},Size: {}, Sequence No: {},Time: {} '.format(key, size, seq, timestamp))
-	 
-#Unpack the ethernet frame
-def ethernet_frame(data):
-        dest_mac,src_mac, proto = struct.unpack('! 6s 6s H',data[:14])
-        return get_mac_addr(dest_mac),get_mac_addr(src_mac), socket.htons(proto), data[14:]
- 
- # formatting the MAC Address to AA:BB:CC:DD:EE:FF   
->>>>>>> b4f917a7a4539e987115dd94fb18d1b0e9225167
 def get_mac_addr(bytes_addr):   
         bytes_str = map('{:02x}'.format, bytes_addr)
         return':'.join(bytes_str).upper()
